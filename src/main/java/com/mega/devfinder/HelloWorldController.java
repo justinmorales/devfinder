c73cc466d54f531a04b7deda5b02480f607983e4
Justin Morales <magagod2@gmail.com>
2024-01-02 22:25:12 -0800

    Hello World printed to localhost:8080

---
+++ src/main/java/com/mega/devfinder/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.mega.devfinder;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello Justin!";
+    }
+
+}
